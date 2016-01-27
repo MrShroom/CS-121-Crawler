b83b02b2ca66c1d53353c1f83ddaeea6d453ffc4
Shaun McThomas <shaunrm23@gmail.com>
2016-01-26 19:40:14 -0800

    MMMMK

--- Assignment 3/src/ir/assignments/three/Crawler.java
+++ Assignment 3/src/ir/assignments/three/Crawler.java
@@ -1,20 +1,134 @@
 package ir.assignments.three;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import edu.uci.ics.crawler4j.url.WebURL;
 
 public class Crawler extends WebCrawler {
 	/**
-	 * This method is for testing purposes only. It does not need to be used
-	 * to answer any of the questions in the assignment. However, it must
-	 * function as specified so that your crawler can be verified programatically.
+	 * This method is for testing purposes only. It does not need to be used to
+	 * answer any of the questions in the assignment. However, it must function
+	 * as specified so that your crawler can be verified programatically.
 	 * 
-	 * This methods performs a crawl starting at the specified seed URL. Returns a
-	 * collection containing all URLs visited during the crawl.
+	 * This methods performs a crawl starting at the specified seed URL. Returns
+	 * a collection containing all URLs visited during the crawl.
 	 */
-	public static Collection<String> crawl(String seedURL) {
+	
+	//main added for testing.
+	public static void main(String [] args){
+		try {
+			PrintWriter out = new PrintWriter("Visited.txt");
+			for(String s : crawl("http://www.ics.uci.edu/")){
+				out.println(s);
+			}
+			out.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//majority of code taken from https://www.ics.uci.edu/~djp3/classes/2014_01_INF141/Discussion/Discussion_03.pdf
+	public static Collection<String> crawl(String seedURL) throws Exception {
 		// TODO implement me
-		return null;
+		hiturls.clear();
+		
+		String crawlStorageFolder = "/Users/shaun/OneDrive/Documents/School/UCI/CS 121/Java Workspace/CS-121-Crawler/Assignment 3/data";//"data/crawl/root";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        
+        //Specific Settings for Project
+        config.setPolitenessDelay(1000);
+        config.setUserAgentString("UCI Inf141-CS121 crawler 82425468 (letzer's here) (shaun's here)");
+        config.setMaxPagesToFetch(1000);
+        config.setMaxDepthOfCrawling(1);
+        
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed(seedURL);
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(Crawler.class, numberOfCrawlers);
+		
+		return hiturls;
+	}
+
+	//taken from https://www.ics.uci.edu/~djp3/classes/2014_01_INF141/Discussion/Discussion_03.pdf
+	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg" + "|png|mp3|mp3|zip|gz))$");
+
+	/**
+	 * This method receives two parameters. The first parameter is the page in
+	 * which we have discovered this new url and the second parameter is the new
+	 * url. You should implement this function to specify whether the given url
+	 * should be crawled or not (based on your crawling logic). In this example,
+	 * we are instructing the crawler to ignore urls that have css, js, git, ...
+	 * extensions and to only accept urls that start with
+	 * "http://www.ics.uci.edu/". In this case, we didn't need the referringPage
+	 * parameter to make the decision.
+	 */
+	
+	
+	//Added to store visited urls.
+	public static List<String> hiturls = new ArrayList<String>();
+	
+	//taken from https://www.ics.uci.edu/~djp3/classes/2014_01_INF141/Discussion/Discussion_03.pdf
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && href.startsWith("http://www.ics.uci.edu/");
+	}
+
+	/**
+	 * This function is called when a page is fetched and ready to be processed
+	 * by your program.
+	 */
+	//taken from https://www.ics.uci.edu/~djp3/classes/2014_01_INF141/Discussion/Discussion_03.pdf
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("URL: " + url);
+		
+		//not taken
+		hiturls.add(url);
+
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			String html = htmlParseData.getHtml();
+			Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+			System.out.println("Text length: " + text.length());
+			System.out.println("Html length: " + html.length());
+			System.out.println("Number of outgoing links: " + links.size());
+		}
 	}
 }
